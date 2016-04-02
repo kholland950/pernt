8ec2c59f3b5ded8047ca55a756d1840a15b3fe21
kholland950 <kholland950@capital.edu>
2016-04-01 23:35:58 -0400

    Implemented different draw tools

--- js/main.js
+++ js/main.js
@@ -1,28 +1,49 @@
-var stage, drawing, oldX, oldY, size, color;
+var stage, size, color;
 
-var mode;
+var shape;
 
 //default select menu item is brush
-var selectedItem;
+var selectedTool;
 var selectedWeight;
 var selectedColor;
 
 var undone = [];
 
-function menuClick(id) {
-}
+var items;
 
 function init() {
-    selectedItem = $("#brush");
+    items = {
+        brush: {
+            button: $("#brush"),
+            strategy: new Brush()
+        },
+        lines: {
+            button: $("#lines"),
+            strategy: new Lines()
+        },
+        polygon: {
+            button: $("#polygon"),
+            strategy: new Polygon()
+        },
+        fill: {
+            button: $("#fill"),
+            strategy: new Fill()
+        },
+        eraser: {
+            button: $("#eraser"),
+            strategy: new Eraser()
+        }
+    };
+
+    selectedTool = items.brush; //brush selected by default
     $(".mt-btn").click(function() {
-        if (this.id == selectedItem[0].id) {
+        if (this.id == selectedTool.button.attr("id")) {
             return;
         }
-        menuClick(this.id);
-        je = $("#" + this.id);
-        je.addClass("btn-large");
-        selectedItem.removeClass("btn-large");
-        selectedItem = je;
+
+        $(this).addClass("btn-large");
+        selectedTool.button.removeClass("btn-large");
+        selectedTool = items[this.id];
     });
 
     $("#weight").click(function() {
@@ -62,11 +83,13 @@ function init() {
         stage.removeChild(shape);
         stage.update();
         undone.push(shape);
+        selectedTool.strategy.undo();
     });
 
     $("#redo").click(function() {
         stage.addChild(undone.pop());
         stage.update();
+        selectedTool.strategy.redo();
     });
 
     $('.modal-trigger').leanModal();
@@ -79,8 +102,6 @@ function init() {
     stage.canvas.height = window.innerHeight - navbarHeight;
     stage.enableDOMEvents(true);
 
-    var shape;
-
     // set up our defaults:
     size = 10;
     selectedWeight = $("#w" + size);
@@ -89,26 +110,19 @@ function init() {
 
     // add handler for stage mouse events:
     stage.on("stagemousedown", function(event) {
-        shape = new createjs.Shape();
-        stage.addChild(shape);
-        drawing = true;
-        undone = []; //kill 'redo' stack
+        selectedTool.strategy.mouseDown(event);
     });
 
     stage.on("stagemouseup", function(event) {
-        drawing = false
+        selectedTool.strategy.mouseUp(event);
     });
 
-    stage.on("stagemousemove",function(evt) {
-        if (oldX && drawing) {
-            shape.graphics.beginStroke(color)
-                .setStrokeStyle(size, "round")
-                .moveTo(oldX, oldY)
-                .lineTo(evt.stageX, evt.stageY);
-            stage.update();
-        }
-        oldX = evt.stageX;
-        oldY = evt.stageY;
+    stage.on("stagemousemove",function(event) {
+        selectedTool.strategy.mouseMove(event);
+    });
+
+    stage.on("dblclick", function(event) {
+        selectedTool.strategy.doubleClick(event);
     });
 
     stage.update();
@@ -124,27 +138,195 @@ function clearCanvas() {
     stage.update();
     closeMenus();
     undone = [];
+    //reset tools
+    items.brush.strategy = new Brush();
+    items.lines.strategy = new Lines();
+    items.polygon.strategy = new Polygon();
+    items.fill.strategy = new Fill();
+    items.eraser.strategy = new Eraser();
 }
 
 //STRATEGY PATTERN ----
 
 function Brush() {
+    var oldX, oldY;
+    var mouseDown = false;
 
+    this.mouseDown = function(event) {
+        shape = new createjs.Shape();
+        stage.addChild(shape);
+
+        shape.graphics.beginFill(color)
+            .drawCircle(event.stageX, event.stageY, size/2);
+        stage.update();
+
+        mouseDown = true;
+        undone = []; //kill 'redo' stack
+    };
+
+    this.mouseUp = function(event) {
+        mouseDown = false
+    };
+
+    this.mouseMove = function(event) {
+        if (oldX && mouseDown) {
+            shape.graphics.beginStroke(color)
+                .setStrokeStyle(size, "round")
+                .moveTo(oldX, oldY)
+                .lineTo(event.stageX, event.stageY);
+            stage.update();
+        }
+        oldX = event.stageX;
+        oldY = event.stageY;
+    };
+
+    this.doubleClick = function(event) {
+
+    }
 }
 
 function Lines() {
+    var oldX, oldY;
 
+    this.mouseDown = function(event) {
+        if (Math.abs(oldX - event.stageX) > 3 || Math.abs(oldY - event.stageY) > 3) {
+            shape = new createjs.Shape();
+            undone = [];
+            stage.addChild(shape);
+
+            shape.graphics.beginStroke(color)
+                .setStrokeStyle(size, "round")
+                .moveTo(oldX, oldY)
+                .lineTo(event.stageX, event.stageY);
+            stage.update();
+        }
+        oldX = event.stageX;
+        oldY = event.stageY;
+    };
+    this.mouseUp = function(event) {
+
+    };
+    this.mouseMove = function(event) {
+
+    };
+    this.doubleClick = function(event) {
+        oldX = undefined;
+        oldY = undefined;
+    };
+    this.undo = function() {
+        oldX = undefined;
+        oldY = undefined;
+    };
+    this.redo = function() {
+        oldX = undefined;
+        oldY = undefined;
+    }
 }
 
 function Polygon() {
+    var lastX, lastY, startX, startY;
+    var shape;
 
+    this.mouseDown = function(event) {
+        if (shape == undefined) {
+            shape = new createjs.Shape();
+            undone = [];
+            stage.addChild(shape);
+            startX = event.stageX;
+            startY = event.stageY;
+        }
+
+        shape.graphics.beginStroke(color)
+            .setStrokeStyle(size, "round")
+            .moveTo(lastX, lastY)
+            .lineTo(event.stageX, event.stageY);
+        stage.update();
+
+        lastX = event.stageX;
+        lastY = event.stageY;
+    };
+    this.mouseUp = function(event) {
+
+    };
+    this.mouseMove = function(event) {
+
+    };
+    this.doubleClick = function(event) {
+        shape.graphics.beginStroke(color)
+            .setStrokeStyle(size, "round")
+            .moveTo(lastX, lastY)
+            .lineTo(startX, startY);
+        stage.update();
+
+        reset();
+    };
+    this.undo = function() {
+        reset();
+    };
+    this.redo = function() {
+        reset();
+    };
+
+    function reset() {
+        shape = undefined;
+        shape = undefined;
+        startX = undefined;
+        startY = undefined;
+        lastX = undefined;
+        lastY = undefined;
+    }
 }
 
 function Fill() {
+    this.mouseDown = function(event) {
 
+    };
+    this.mouseUp = function(event) {
+
+    };
+    this.mouseMove = function(event) {
+
+    };
+    this.doubleClick = function(event) {
+
+    }
 }
 
 function Eraser() {
+    var white = "#fff";
+    var oldX, oldY;
+    var mouseDown = false;
 
+    this.mouseDown = function(event) {
+        shape = new createjs.Shape();
+        stage.addChild(shape);
+
+        shape.graphics.beginFill(white)
+            .drawCircle(event.stageX, event.stageY, size/2);
+        stage.update();
+
+        mouseDown = true;
+        undone = []; //kill 'redo' stack
+    };
+
+    this.mouseUp = function(event) {
+        mouseDown = false
+    };
+
+    this.mouseMove = function(event) {
+        if (oldX && mouseDown) {
+            shape.graphics.beginStroke(white)
+                .setStrokeStyle(size, "round")
+                .moveTo(oldX, oldY)
+                .lineTo(event.stageX, event.stageY);
+            stage.update();
+        }
+        oldX = event.stageX;
+        oldY = event.stageY;
+    };
+
+    this.doubleClick = function(event) {
+
+    }
 }
 //---------
